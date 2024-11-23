c8a349ed12f58d92b11a8fcd36a057af9eb89422
deyson34 <deyson.flores@tecsup.edu.pe>
2024-11-22 21:35:43 -0500

    Ultima Actualizacion backend

--- demo/src/main/java/com/example/demo/controller/GeminiController.java
+++ demo/src/main/java/com/example/demo/controller/GeminiController.java
@@ -1,8 +1,10 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.service.GeminiService;
 
 @RestController
 @RequestMapping("/api")
